618ed16d9885ed124d1c61f405dc9818fbc1b12e
Komamitsu <komamitsu@gmail.com>
2010-03-22 23:58:33 +0900

    fixed dijkstra.c

--- dijkstra.c
+++ dijkstra.c
@@ -1,87 +1,70 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
 
+#define MAX_BRANCH 10
 typedef struct {
-  char from;
-  char to;
+  char id;
+  char next_ids[MAX_BRANCH];
+  int next_costs[MAX_BRANCH];
   int cost;
-  int selected;
+  char from;
 } route;
 
 route routes[] = {
-  { 'S', 'A', 5, 0 },
-  { 'S', 'B', 4, 0 },
-  { 'S', 'C', 2, 0 },
-  { 'A', 'B', 2, 0 },
-  { 'A', 'G', 6, 0 },
-  { 'B', 'C', 3, 0 },
-  { 'B', 'D', 2, 0 },
-  { 'C', 'B', 3, 0 },
-  { 'C', 'D', 6, 0 },
-  { 'D', 'G', 4, 0 }
+  { 'S', {'A', 'B', 'C'}, {5, 4, 2}, 0 },
+  { 'A', {'B', 'G'}, {2, 6}, -1 },
+  { 'B', {'C', 'D'}, {3, 2}, -1 },
+  { 'C', {'B', 'D'}, {3, 6}, -1 },
+  { 'D', {'G'}, {4}, -1 },
+  { 'G', {}, {}, -1 }
 };
 
-typedef struct trace {
-  char last_pos;
-  struct trace *last_trace;
-  route *route;
-  int cost;
-} trace;
+const int routes_len = sizeof(routes) / sizeof(routes[0]);
 
-trace traces[20] = {
-  { 'S', NULL, NULL, 0 }
-};
+route *find_route(char id)
+{
+  int i;
+  for (i = 0; i < routes_len; i++) {
+    if (routes[i].id == id) return &routes[i];
+  }
+  return NULL;
+}
 
 int main()
 {
   int i, j;
-  int tmp_cost;
-  trace *trace_candidate;
-  route *route_candidate;
-  char last_pos;
+  char from, to;
+  route *next_route;
+  const int large_cost = 99999;
 
   while (1) {
-    trace_candidate = NULL;
-    route_candidate = NULL;
-    tmp_cost = 999999;
+    for (i = 0; i < routes_len; i++) {
+      if (routes[i].cost >= 0) {
+        for (j = 0; j < sizeof(routes[i].next_ids) / 
+                        sizeof(routes[i].next_ids[0]); j++) {
 
-    for (i = 0; i < sizeof(traces) / sizeof(traces[0]); i++) {
-      if (traces[i].last_pos) {
-        for (j = 0; j < sizeof(routes) / sizeof(routes[0]); j++) {
-          if (routes[j].selected) continue;
-          if (traces[i].last_pos == routes[j].from &&
-              traces[i].cost + routes[j].cost < tmp_cost) {
-            trace_candidate = &traces[i];
-            route_candidate = &routes[j];
-            tmp_cost = traces[i].cost + routes[j].cost;
+          if (!routes[i].next_ids[j]) break;
+
+          next_route = find_route(routes[i].next_ids[j]);
+
+          if (routes[i].cost + routes[i].next_costs[j] < next_route->cost ||
+              next_route->cost < 0) {
+            next_route->cost = routes[i].cost + routes[i].next_costs[j];
+            next_route->from = routes[i].id;
           }
         }
       }
     }
 
-    for (i = 0; i < sizeof(traces) / sizeof(traces[0]); i++) {
-      if (!traces[i].last_pos) {
-        traces[i].last_pos = route_candidate->to;
-        traces[i].last_trace = trace_candidate;
-        traces[i].route = route_candidate;
-        traces[i].cost = trace_candidate->cost + route_candidate->cost;
-        route_candidate->selected = 1;
-        break;
-      }
+    for (i = 0; i < routes_len; i++) {
+      if (routes[i].cost < 0) break;
     }
-    if (route_candidate->to == 'G') break;
+    if (i >= routes_len) break;
   }
 
-  last_pos = 'G';
-  while (1) {
-    for (i = 0; i < sizeof(traces) / sizeof(traces[0]); i++) {
-      if (traces[i].route && traces[i].route->to == last_pos) break;
-    }
-    printf("from: %c, to: %c\n", traces[i].route->from, traces[i].route->to);
-    last_pos = traces[i].route->from;
-    if (last_pos == 'S') break;
+  for (i = 0; i < routes_len; i++) {
+    printf("id(%c) from(%c) cost(%d)\n", routes[i].id, routes[i].from, routes[i].cost);
   }
 
   return 0;
 }
+
