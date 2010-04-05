18f8fe0b431763bee27a49976b790052e3d934fa
Komamitsu <komamitsu@gmail.com>
2010-04-05 22:47:58 +0900

    fixs the file name conflict of pe8.ml

--- pe/pe8.ml
+++ pe/pe8.ml
@@ -1,39 +1,46 @@
-type pos = A | B | C
+let strtab = [|
+  "73167176531330624919225119674426574742355349194934";
+  "96983520312774506326239578318016984801869478851843";
+  "85861560789112949495459501737958331952853208805511";
+  "12540698747158523863050715693290963295227443043557";
+  "66896648950445244523161731856403098711121722383113";
+  "62229893423380308135336276614282806444486645238749";
+  "30358907296290491560440772390713810515859307960866";
+  "70172427121883998797908792274921901699720888093776";
+  "65727333001053367881220235421809751254540594752243";
+  "52584907711670556013604839586446706324415722155397";
+  "53697817977846174064955149290862569321978468622482";
+  "83972241375657056057490261407972968652414535100474";
+  "82166370484403199890008895243450658541227588666881";
+  "16427171479924442928230863465674813919123162824586";
+  "17866458359124566529476545682848912883142607690042";
+  "24219022671055626321111109370544217506941658960408";
+  "07198403850962455444362981230987879927244284909188";
+  "84580156166097919133875499200524063689912560717606";
+  "05886116467109405077541002256983155200055935729725";
+  "71636269561882670428252483600823257530420752963450";
+|]
 
-let pw i = int_of_float ((float_of_int i) ** 2.0)
-
-let calc sum =
-  let rec loop pos a b c =
-    let is_pn = (pw a) + (pw b) = (pw c) in
-    let total = a + b + c in
-    if total = sum && is_pn then Some (a, b, c)
-    else (
-      if total > sum then None
-      else (
-        match pos with
-        | C -> loop C a b (c + 1)
-        | B -> (
-            match (loop B a (b + 1) (c + 1), loop C a b (c + 1)) with
-            | None, None -> None
-            | Some x, _ -> Some x
-            | _, Some x -> Some x
-        )
-        | A -> (
-            match (loop A (a + 1) (b + 1) (c + 1), loop B a (b + 1) (c + 1)) with
-            | None, None -> None
-            | Some x, _ -> Some x
-            | _, Some x -> Some x
-        )
-      )
-    )
-  in
-  loop A 1 2 3
+let str = Array.fold_left (fun acc x -> acc ^ x) "" strtab
 
 let _ =
-  let a, b, c = 
-    match calc 1000 with
-    | Some (a, b, c) -> a, b, c
-    | None -> failwith "not found"
-  in
-  Printf.printf "a:%d, b:%d, c:%d\n" a b c
+  let n = String.length str in
+  let to_i c = int_of_string (Char.escaped c) in
+  let rec loop max max_str i =
+    if i > n - 4 - 1 then (max, max_str)
+    else (
+      let prod =
+        (to_i str.[i]) * 
+        (to_i str.[i + 1]) * 
+        (to_i str.[i + 2]) * 
+        (to_i str.[i + 3]) * 
+        (to_i str.[i + 4]) in
+      let max, max_str = 
+        if prod > max then (prod, String.sub str i 5) 
+        else (max, max_str) in
+      loop max max_str (i + 1)
+    ) in
+  let max, max_str = loop 0 "" 0 in
+  Printf.printf "max:%d max_str:%s\n" max max_str
+
 
