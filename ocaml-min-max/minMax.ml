c278d1781bd40602e38faf79de2a534fba426cb6
Mitsunori Komatsu <komamitsu@gmail.com>
2010-05-07 23:24:57 +0900

    use functor

--- ocaml-min-max/minMax.ml
+++ ocaml-min-max/minMax.ml
@@ -1,66 +1,73 @@
-module MinMax :
+module Player =
+  struct
+    type t = First | Last
+    let next_player = function First -> Last | Last -> First
+  end
+
+module type GameType =
   sig
-    type player = First | Last
-    type 'a t
-    type 'a result = {player:player; score:int; condition:'a; children:'a result list}
+    type t
+    val find : Player.t -> t -> int -> t list
+    val eval : Player.t -> t -> int -> int option
+  end
 
-    val create :
-        (player -> 'a -> int -> 'a list) ->
-        (player -> 'a -> int -> int option) -> 'a t
-
-    val eval : 'a t -> player -> 'a -> 'a result
+module MakeMinMax (Game : GameType) :
+  sig
+    type gt = Game.t
+    type result = {player:Player.t; score:int; condition:gt; children:result list}
+    val eval : Player.t -> gt -> result
   end
 =
   struct
-  type player = First | Last
-  type 'a t =
-    {finder: player -> 'a -> int -> 'a list;
-     evaluator: player -> 'a -> int -> int option}
+    type gt = Game.t
 
-  type 'a result = {player:player; score:int; condition:'a; children:'a result list}
+    type result = {player:Player.t; score:int; condition:gt; children:result list}
 
-  let create finder evaluator = {finder = finder; evaluator = evaluator}
-
-  let next_player = function First -> Last | Last -> First
-
-  let eval t player condition = 
-    let rec _eval player condition depth =
-      match t.evaluator player condition depth with
-      | Some score ->
+    let eval player condition = 
+      let rec _eval player condition depth =
+        match Game.eval player condition depth with
+        | Some score ->
+            {player = player; score = score; 
+             condition = condition; children = []}
+        | None ->
+          let next_player = Player.next_player player in
+          let next_choices = Game.find player condition depth in
+          let children =
+            List.fold_left
+              (fun result nc -> (_eval next_player nc (depth + 1))::result)
+              [] next_choices in
+          let init, compare = 
+            match player with
+            | Player.First -> (min_int, max)
+            | Player.Last  -> (max_int, min) in
+          let score = 
+            List.fold_left 
+              (fun s child -> compare s child.score) init children in
           {player = player; score = score; 
-           condition = condition; children = []}
-      | None ->
-        let next_player = next_player player in
-        let next_choices = t.finder player condition depth in
-        let children =
-          List.fold_left
-            (fun result nc -> (_eval next_player nc (depth + 1))::result)
-            [] next_choices in
-        let init, compare = 
-          match player with
-          | First -> (min_int, max)
-          | Last  -> (max_int, min) in
-        let score = 
-          List.fold_left 
-            (fun s child -> compare s child.score) init children in
-        {player = player; score = score; 
-         condition = condition; children = children}
-    in
-    _eval player condition 0
-end
+           condition = condition; children = children}
+      in
+      _eval player condition 0
+  end
 
-let _ =
-  let counter = ref 1 in
-  let mm =
-    MinMax.create
-      (fun player cond depth ->
+module SampleMinMax = 
+  MakeMinMax(
+    struct
+      type t = int
+
+      let counter = ref 1
+
+      let find player cond depth =
         let rec loop i choises =
           if i > depth then choises
           else loop (i + 1) (((i * depth * !counter) mod 7)::choises)
         in
         counter := !counter + 1;
-        loop 0 [])
-      (fun player cond depth ->
-        if depth > 2 then Some cond else None) in
-  MinMax.eval mm MinMax.First 4
+        loop 0 [] 
 
+      let eval player cond depth =
+        if depth > 2 then Some cond else None
+    end
+  )
+
+let _ =
+  SampleMinMax.eval Player.First 4
