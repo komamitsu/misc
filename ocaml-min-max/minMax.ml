631a464d23e5b2dc244f2f11e4725453d233a612
Mitsunori Komatsu <komamitsu@gmail.com>
2010-05-12 23:52:53 +0900

    refactored.

--- ocaml-min-max/minMax.ml
+++ ocaml-min-max/minMax.ml
@@ -4,131 +4,83 @@ module Player =
     let next_player = function First -> Last | Last -> First
   end
 
-module type GameType =
-  sig
-    type t
-    val find : Player.t -> t -> int -> t list
-    val eval : Player.t -> t -> int -> int option
-    val to_string : t -> string
-  end
+open Player
+open Printf
 
-module MakeMinMax (Game : GameType) :
-  sig
-    type gt = Game.t
-    type result = {player:Player.t; score:int; condition:gt; children:result list}
-    val eval : Player.t -> gt -> result
-    val eval_with_alpha_beta_cut : Player.t -> gt -> result
-  end
-=
-  struct
-    type gt = Game.t
+type 'a result = {
+  player:Player.t;
+  score:int;
+  children:('a * 'a result) list
+}
 
-    type result = {player:Player.t; score:int; condition:gt; children:result list}
+let eval player condition finder evaluater = 
+  let rec _eval player condition depth =
+    match evaluater player condition depth with
+    | Some score -> {player = player; score = score; children = []}
+    | None ->
+        let next_player = next_player player in
+        let next_choices = finder player condition depth in
+        let children =
+          List.fold_left
+            (fun rl (nc, new_cond) -> 
+              (nc, _eval next_player new_cond (depth + 1))::rl)
+            [] next_choices in
+        let init, compare = 
+          match player with
+          | First -> (min_int, max)
+          | Last  -> (max_int, min) in
+        let score = 
+          List.fold_left 
+            (fun s (choice, child) -> compare s child.score) 
+            init children in
+        {player = player; score = score; children = children}
+  in
+  _eval player condition 0
 
-    let eval player condition = 
-      let rec _eval player condition depth =
-        match Game.eval player condition depth with
-        | Some score ->
-            {player = player; score = score; 
-             condition = condition; children = []}
-        | None ->
-          let next_player = Player.next_player player in
-          let next_choices = Game.find player condition depth in
-          let children =
-            List.fold_left
-              (fun result nc -> (_eval next_player nc (depth + 1))::result)
-              [] next_choices in
-          let init, compare = 
-            match player with
-            | Player.First -> (min_int, max)
-            | Player.Last  -> (max_int, min) in
-          let score = 
-            List.fold_left 
-              (fun s child -> compare s child.score) init children in
-          {player = player; score = score; 
-           condition = condition; children = children}
-      in
-      _eval player condition 0
+let eval_with_alpha_beta player condition finder evaluater = 
+  let rec _eval player condition depth alpha beta =
+    match evaluater player condition depth with
+    | Some score -> {player = player; score = score; children = []}
+    | None ->
+        let next_player = next_player player in
+        let next_choices = finder player condition depth in
+        let children, alpha, beta =
+          List.fold_left
+            (fun (rl, alpha, beta) (nc, new_cond) -> 
+              if alpha >= beta then (rl, alpha, beta)
+              else
+                let child =
+                  _eval next_player new_cond (depth + 1) alpha beta in
+                let score = child.score in
+                match player with
+                | First when score > alpha -> ((nc, child)::rl, score, beta)
+                | Last  when score < beta  -> ((nc, child)::rl, alpha, score)
+                | _                        -> ((nc, child)::rl, alpha, beta)
+            )
+            ([], alpha, beta) next_choices in
+        let score =
+          match player with First -> alpha | Last -> beta in
+        {player = player; score = score; children = children}
+  in
+  _eval player condition 0 min_int max_int
 
-    let eval_with_alpha_beta_cut player condition = 
-      let rec _eval player condition depth alpha beta =
-(*
-Printf.printf "#1 player[%s] depth[%d] condition[%s] alpha[%d] beta[%d]\n"
-  (if player = Player.First then "first" else "last ")
-  depth (Game.to_string condition) alpha beta;
-*)
-        match Game.eval player condition depth with
-        | Some score ->
-            {player = player; score = score; 
-             condition = condition; children = []}
-        | None ->
-          let next_player = Player.next_player player in
-          let next_choices = Game.find player condition depth in
-          let children, alpha, beta =
-            List.fold_left
-              (fun status nc -> 
-                let children, alpha, beta = status in
-(*
-Printf.printf "child: alpha[%d] beta[%d]\n" alpha beta;
-*)
-                if alpha < beta then (
-                  let child = _eval next_player nc (depth + 1) alpha beta in
-                  let children = child::children in
-                  match player with
-                  | Player.First when child.score > alpha ->
-                      (children, child.score, beta)
-                  | Player.Last  when child.score < beta  ->
-                      (children, alpha, child.score)
-                  | _ -> (children, alpha, beta)
-                )
-                else (
-(*
-Printf.printf "Cut!!!\n";
-*)
-                  status
-                )
-              )
-              ([], alpha, beta) next_choices in
-(*
-Printf.printf "#2 player[%s] depth[%d] condition[%s] alpha[%d] beta[%d]\n"
-  (if player = Player.First then "first" else "last ")
-  depth (Game.to_string condition) alpha beta;
-*)
-          {player = player;
-           score = if player = Player.First then alpha else beta;
-           condition = condition; children = children}
-      in
-      _eval player condition 0 min_int max_int
-  end
-
-(*
-module SampleMinMax = 
-  MakeMinMax(
-    struct
-      type t = int
-
-      let i = ref (-2)
-      let ii = ref (100 - 2)
-      (* let nums = [|1;2;3;4;5;6;7;8|] *)
-      let nums = [|8;7;6;5;4;3;2;1|]
-
-      let find player cond depth =
+let _ =
+  let i = ref (-2) in
+  let ii = ref (100 - 2) in
+  (* let nums = [|1;2;3;4;5;6;7;8|] in *)
+  let nums = [|5;6;7;8;4;3;2;1|] in
+  eval_with_alpha_beta First 777
+    (* find next choices *)
+    (fun player condition depth ->
         if depth >= 2 then (
           i := !i + 2;
-          [nums.(!i); nums.(!i + 1)]
+          [(!i + 200, nums.(!i)); (!i + 201, nums.(!i + 1))]
         )
         else (
           ii := !ii + 2;
-          [!ii; !ii + 1]
-        )
+          [(!ii + 200, !ii); (!ii + 201, !ii)]
+        ))
+    (* evaluate the condition *)
+    (fun player condition depth ->
+      if depth > 2 then Some condition else None)
 
-      let eval player cond depth =
-        if depth > 2 then Some cond else None
-
-      let to_string i = string_of_int i
-    end
-  )
-
-let _ =
-  SampleMinMax.eval_with_alpha_beta_cut Player.First 7
-*)
